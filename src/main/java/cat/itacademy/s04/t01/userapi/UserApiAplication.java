e78d4fbf181882e21f630a0f7b7b8b7ed4d2ac73
Ana Ruth <aniarinna@gmail.com>
2026-03-04 11:50:49 +0100

    feat: add health endpoint with json response

---
+++ src/main/java/cat/itacademy/s04/t01/userapi/UserApiAplication.java
@@ -0,0 +1,12 @@
+package cat.itacademy.s04.t01.userapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApiAplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApiAplication.class, args);
+    }
+}
---
+++ src/test/java/cat/itacademy/s04/t01/userapi/HealthControllerTest.java
@@ -0,0 +1,4 @@
+package cat.itacademy.s04.t01.userapi;
+
+public class HealthControllerTest {
+}
