ab544f92d8e0cde1d414be4ddd602746063c0785
Ana Ruth <aniarinna@gmail.com>
2026-03-22 07:54:44 +0100

    fix: update tests and verifi executable jar

--- src/test/java/cat/itacademy/s04/t01/userapi/HealthControllerTest.java
+++ src/test/java/cat/itacademy/s04/t01/userapi/HealthControllerTest.java
@@ -1,5 +1,6 @@
 package cat.itacademy.s04.t01.userapi;
 
+import cat.itacademy.s04.t01.userapi.controllers.HealthController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -8,8 +9,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-@WebMvcTest
-public class HealthControllerTest {
+
+@WebMvcTest(HealthController.class)
+class HealthControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -18,6 +20,6 @@ public class HealthControllerTest {
     void shouldReturnOkStatus() throws Exception {
         mockMvc.perform(get("/health"))
                 .andExpect(status().isOk())
-        .andExpect(jsonPath("$.status").value("OK"));
+                .andExpect(jsonPath("$.status").value("OK"));
     }
 }
